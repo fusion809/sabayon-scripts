a7b88647f2352b0192468934a75986f2f62357a0
fusion809 <brentonhorne77@gmail.com>
2015-11-26 00:50:44 +1000

    Updating scripts

--- Shell/chroot.sh
+++ Shell/chroot.sh
@@ -52,32 +52,58 @@ function chrootb {
 }
 
 function sabayon-mirror {
-  L=(
-  'Argentina                         - ftp://mirrors.coopvgg.com.ar/sabayon'
-  'Austria                           - ftp://gd.tuwien.ac.at/linux/sabayonlinux'
-  'Australia (Optus, 2,000 MB/s)     - ftp://mirror.optusnet.com.au/sabayon'
-  'Australia (Internode, 1,000 MB/s) - ftp://mirror.internode.on.net/pub/sabayon'
-  'Belgium                           - ftp://ftp.belnet.be/mirror/sabayonlinux'
-  'Brazil                            - ftp://sabayon.c3sl.ufpr.br/sabayon'
-  'Czech Republic                    - ftp://mirror.dkm.cz/pub/sabayon'
-  'Denmark                           - ftp://ftp.klid.dk/sabayonlinux'
-  'Ecuador                           - ftp://mirror.uta.edu.ec/sabayon'
-  'Germany (Official)                - http://mirror.de.sabayon.org'
-  'Greece                            - ftp://ftp.cc.uoc.gr/mirrors/linux/SabayonLinux'
-  'Italy (Garr Consortium)           - ftp://na.mirror.garr.it/mirrors/sabayonlinux'
-  'Italy (TOP-IX Official Mirror)    - http://mirror.it.sabayon.org'
-  'Japan (RIKEN, 10,000 MB/s)        - ftp://ftp.riken.jp/Linux/sabayon'
-  'Netherlands                       - ftp://ftp.nluug.nl/pub/os/Linux/distr/sabayonlinux'
-  'New Zealand                       - http://mirror.auckland.ac.nz/sabayon'
-  'Portugal (Rede, 1,000 MB/s)       - ftp://ftp.rnl.tecnico.ulisboa.pt/pub/sabayon'
-  'Russian Federation                - ftp://mirror.yandex.ru/sabayon'
-  'South Africa                      - ftp://sabayon.mirror.ac.za'
-  'Sweden                            - ftp://ftp.portlane.com/pub/os/linux/sabayon'
-  'U.S.A (UMD, 10,000 MB/s)          - http://mirror.umd.edu/sabayonlinux'
+  ## Declare the associative array
+  declare -A L=(
+  [Argentina 1]="ftp://mirrors.coopvgg.com.ar/sabayon"                            [Argentina 2]="http://mirrors.coopvgg.com.ar/sabayon"
+  [Austria 1]="ftp://gd.tuwien.ac.at/linux/sabayonlinux"                          [Austria 2]="http://gd.tuwien.ac.at/linux/sabayonlinux"
+  [Australia 1 FTP Internode]="ftp://mirror.internode.on.net/pub/sabayon"         [Australia 2 HTTP Internode]="http://mirror.internode.on.net/pub/sabayon"
+  [Australia 3 FTP OptusNet]="ftp://mirror.optusnet.com.au/sabayon"               [Australia 4 FTP OptusNet]="http://mirror.optusnet.com.au/sabayon"
+  [Belgium 1]="ftp://ftp.belnet.be/mirror/sabayonlinux"                           [Belgium 2]="http://ftp.belnet.be/mirror/sabayonlinux"
+  [Brazil 1]="ftp://sabayon.c3sl.ufpr.br/sabayon"                                 [Brazil 2]="http://sabayon.c3sl.ufpr.br"
+  [Czech Republic 1]="ftp://mirror.dkm.cz/pub/sabayon"                            [Czech Republic 2]="http://sabayon.mirror.dkm.cz/pub/sabayon"
+  [Germany 1]="http://mirror.de.sabayon.org"                                      [Denmark 1]="ftp://ftp.klid.dk/sabayonlinux/enttropy"
+  [Denmark 2]="http://ftp.klid.dk/sabayonlinux"                                   [Greece 1]="ftp://ftp.cc.uoc.gr/mirrors/linux/SabayonLinux"
+  [Greece 2]="http://ftp.cc.uoc.gr/mirrors/linux/SabayonLinux"                    [Hungary 1]="ftp://ftp.fsn.hu/pub/linux/distributions/sabayon"
+  [Hungary 2]="http://ftp.fsn.hu/pub/linux/distributions/sabayon"                 [Italy 1]="http://mirror.it.sabayon.org"
+  [Italy 2]="ftp://na.mirror.garr.it/mirrors/sabayonlinux"                        [Italy 3]="http://na.mirror.garr.it/mirrors/sabayonlinux"
+  [Japan 1]="ftp://ftp.riken.jp/Linux/sabayon"                                    [Japan 2]="http://ftp.riken.jp/Linux/sabayon"
+  [Japan 3]="ftp://ftp.kddilabs.jp/Linux/packages/sabayonlinux"                   [Japan 4]="http://ftp.kddilabs.jp/Linux/packages/sabayonlinux"
+  [Japan 5]="http://ftp.tsukuba.wide.ad.jp/Linux/sabayon"                         [Netherlands 1]="ftp://ftp.nluug.nl/pub/os/Linux/distr/sabayonlinux"
+  [Netherlands 2]="http://ftp.nluug.nl/os/Linux/distr/sabayonlinux"               [Netherlands 3]="ftp://ftp.surfnet.nl/pub/os/Linux/distr/sabayonlinux"
+  [Portugal 1]="ftp://glua.ua.pt/sabayon"                                         [Portugal 2]="ftp://ftp.rnl.ist.utl.pt/pub/sabayon"
+  [Portugal 3]="http://ftp.rnl.ist.utl.pt/pub/sabayon"                            [Russian Federation 1]="ftp://mirror.yandex.ru/sabayon"
+  [Russian Federation 2]="http://mirror.yandex.ru/sabayon"                        [Sweden 1]="ftp://ftp.portlane.com/pub/os/linux/sabayon"
+  [Sweden 2]="http://ftp.portlane.com/pub/os/linux/sabayon"                       [United States 1]="ftp://mirrors-usa.go-parts.com/sabayon"
+  [United States 2]="http://mirrors-usa.go-parts.com/sabayon"                     [United States 3]="ftp://mirror.cs.vt.edu/pub/SabayonLinux"
+  [United States 6]="http://mirror.clarkson.edu/sabayon"                          [United States 7]="http://mirror.umd.edu/sabayonlinux"
+  [United States 4]="http://mirror.cs.vt.edu/pub/SabayonLinux"                    [United States 5]="http://cross-lfs.sabayonlinux.org"
+  [South Africa 1]="ftp://sabayon.mirror.ac.za"                                   [South Africa 2]="http://sabayon.mirror.ac.za"
   )
-  select x in "${L[@]}"
+  ## Unfortunately, associative arrays are not stored in the
+  ## order you create them with so, to have the select show
+  ## sorted options, we need a second, helper array.
+  sorted=(
+  [1]="Argentina 1"                            [2]="Argentina 2"                            [3]="Australia 1 FTP Internode"
+  [4]="Australia 2 HTTP Internode"             [5]="Australia 3"                            [6]="Australia 4"
+  [7]="Austria 1"                              [8]="Austria 2"                              [9]="Belgium 1"
+  [10]="Belgium 2"                             [11]="Brazil 1"                              [12]="Brazil 2"
+  [13]="Czech Republic 1"                      [14]="Czech Republic 2"                      [15]="Denmark 1"
+  [16]="Denmark 2"                             [17]="Germany 1"                             [18]="Greece 1"
+  [19]="Greece 2"                              [20]="Hungary 1"                             [21]="Hungary 2"
+  [22]="Italy 1"                               [23]="Italy 2"                               [24]="Italy 3"
+  [25]="Japan 1"                               [26]="Japan 2"                               [27]="Japan 3"
+  [28]="Japan 4"                               [29]="Japan 5"                               [30]="Netherlands 1"
+  [31]="Netherlands 2"                         [32]="Netherlands 3"                         [33]="Portugal 1"
+  [34]="Portugal 2"                            [35]="Portugal 3"                            [36]="Russian Federation 1"
+  [37]="Russian Federation 2"                  [38]="South Africa 1"                        [39]="South Africa 2"
+  [40]="Sweden 1"                              [41]="Sweden 2"                              [42]="United States 1"
+  [43]="United States 2"                       [44]="United States 3"                       [45]="United States 4"
+  [46]="United States 5"                       [47]="United States 6"                       [48]="United States 7"
+  )
+  select x in "${sorted[@]}"
   do
-    export MIRROR="${x#*-}"
+      export MIRROR="${L[$x]}"
+      break
   done
 }
 
@@ -89,12 +115,9 @@ function sabayon-chrootn {
       mkdir $HOME/chroots
       # Enter the chroots directory
       pushd "$HOME/chroots"
-        # Select a mirror from which to download the tarball. I have chosen this
-        # mirror as it is the best one available for Australian Sabayon users, like
-        # myself. If you reside in another country, you should choose a mirror
-        # closest to you! These tarballs are usually >500 MB in size, so closer
+        # Select a mirror from which to download the tarball.
+        # These tarballs are usually >500 MB in size, so closer
         # the mirror, the better.
-        MIRROR=ftp://mirror.optusnet.com.au/sabayon
         wget -c $MIRROR/iso/daily/Sabayon_Linux_DAILY_amd64_tarball.tar.gz
         sudo mkdir /root$1
         sudo cp -a "Sabayon_Linux_DAILY_amd64_tarball.tar.gz" /root$1
